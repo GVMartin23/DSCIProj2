a1a216ed4ff454f94952d22225fe22b827ef483d
GVMartin23 <MartinGV19@gcc.edu>
2024-04-29 09:48:50 -0400

    script7

--- script7.py
+++ script7.py
@@ -4,8 +4,9 @@ from pyspark.sql.functions import col,to_date
 spark = SparkSession.builder.appName("Script7").getOrCreate()
 spark.sparkContext.setLogLevel('WARN')
 
-df = spark.read.option("inferSchema", True).option('header',True).csv(r"proj2_data\orders_sample.csv")
+orderMulti = spark.read.option("inferSchema", True).option('header',True).csv(r"proj2_data\product_sample1.csv")
+products = spark.read.option("inferSchema", True).option('header',True).csv(r"proj2_data\products1.csv")
 
-df.where('userId==3').select('orderId').show()
+orderMulti.join(products, on=["productId"], how="left").groupBy("orderId", "aisleId").count().select('aisleId').groupBy("aisleId").count().show()
 
 spark.stop()

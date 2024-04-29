e9dd157a4a771a9522091d0d10fc6d9cedafc747
GVMartin23 <MartinGV19@gcc.edu>
2024-04-29 08:59:11 -0400

    script template for each script

--- script1.py
+++ script1.py
@@ -1,5 +1,4 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col,to_date
 
 spark = SparkSession.builder.appName("Script1").getOrCreate()
 spark.sparkContext.setLogLevel('WARN')
---
+++ script3.py
@@ -0,0 +1,11 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col,to_date
+
+spark = SparkSession.builder.appName("Script1").getOrCreate()
+spark.sparkContext.setLogLevel('WARN')
+
+df = spark.read.option("inferSchema", True).option('header',True).csv(r"proj2_data\orders_sample.csv")
+
+df.where('userId==3').select('orderId').show()
+
+spark.stop()
